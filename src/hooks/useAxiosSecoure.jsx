e88a00dedd23a301555ebd834c4d1966037705e4
TUMIT_HASAN <tumithasan1@gmail.com>
2025-06-30 15:25:02 +0600

    create the jwt token on the client side

--- src/hooks/useAxiosSecoure.jsx
+++ src/hooks/useAxiosSecoure.jsx
@@ -1,11 +1,24 @@
 import axios from 'axios';
 import React from 'react';
+import useAuth from './useAuth';
 
 const axiosSecoure = axios.create({
-    baseURL:`http://localhost:5000`
+    baseURL: `http://localhost:5000`
 })
 
 const useAxiosSecoure = () => {
+
+    const { user } = useAuth()
+
+    axiosSecoure.interceptors.request.use(config => {
+        config.headers.Authorization = `bearer ${user.accessToken}`
+        return config;
+    }, error => {
+        return Promise.reject(error)
+    })
+
+
+
     return axiosSecoure;
 };
 
