c87b7cb576718380833dbcfab22dcc85c28d4b83
TUMIT_HASAN <tumithasan1@gmail.com>
2025-07-07 02:10:25 +0600

    added more sicoure the fetching data

--- src/hooks/useAxiosSecoure.jsx
+++ src/hooks/useAxiosSecoure.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecoure = axios.create({
     baseURL: `https://zap-shift-server-three.vercel.app`
@@ -8,7 +9,9 @@ const axiosSecoure = axios.create({
 
 const useAxiosSecoure = () => {
 
-    const { user } = useAuth()
+    const { user, signOutUser } = useAuth();
+    const navigate = useNavigate()
+
 
     axiosSecoure.interceptors.request.use(config => {
         config.headers.Authorization = `bearer ${user.accessToken}`
@@ -17,7 +20,22 @@ const useAxiosSecoure = () => {
         return Promise.reject(error)
     })
 
-
+    axiosSecoure.interceptors.response.use(res => {
+        return res;
+    }, error => {
+        const status = error.status;
+        if (status === 403) {
+            navigate('/forbidden')
+        }
+        else if (status === 401) {
+            signOutUser()
+                .then(() => {
+                    navigate('/auth/login')
+                })
+                .catch(() => { })
+        }
+        return Promise.reject(error)
+    })
 
     return axiosSecoure;
 };
